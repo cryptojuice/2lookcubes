fc578c931f54451f38ef3e5e739cfd3d78752d0a
Nick Robinson <nick@shakelaw.com>
2015-01-24 20:29:56 -0500

    development server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+  res.location('/#' + req.path())
+  .status(301)
+  .send();
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('Express app listening at http://%s:%s', host, port);
+});
