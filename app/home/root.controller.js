46d9813732bd030e5f519cba199b2d2e51b4c62e
Nick Robinson <grobins2@gmail.com>
2015-01-25 20:30:38 -0500

    Complete OLL PLL views

--- app/home/root.controller.js
+++ app/home/root.controller.js
@@ -1,5 +1,4 @@
 cubeApp.controller('RootController', [ '$scope', function($scope) {
-  console.log('in root controller');
   $scope.ollList = {
     edge: [
       { name: 'Opposite', image: 'opposite.png', algorithms: ["F (R U R' U') F'"] },
@@ -14,6 +13,19 @@ cubeApp.controller('RootController', [ '$scope', function($scope) {
       { name: 'Headlights', image: 'Headlights.png', algorithms: ["(R2 D)(R' U2)(R D')(R' U2 R')"] },
       { name: 'Chameleon', image: 'Chemeleon.png', algorithms: ["(r U R' U')(r' F R F')"] },
       { name: 'Bowtie', image: 'Bowtie.png', algorithms: ["F'(r U R' U')(r' F R)"] }
+    ]
+  };
+
+  $scope.pllList = {
+    corner: [
+      { name: 'CW Corner 3-Cycle', image: 'cw-corner-3-cycle.png', algorithms: ["x [(R' U R') D2] [(R U' R') D2] R2"] },
+      { name: 'E-Perm', image: 'e-perm.png', algorithms: ["x' [(R U' R') D (R U R')] u2 [(R' U R) D (R' U' R)]"] },
+    ],
+    edge: [
+      { name: 'CW Edge 3-Cycle', image: 'cw-edge-3-cycle.png', algorithms: ["R2 U (R U R' U')(R' U')(R' U R')"] },
+      { name: 'CCW Edge 3-Cycle', image: 'ccw-edge-3-cycle.png', algorithms: ["(R U')(R U R U)(R U')(R' U' R2)"] },
+      { name: 'H-Perm', image: 'h-perm.png', algorithms: ["(M2 U) (M2 U2 M2) (U M2)"] },
+      { name: 'Z-Perm', image: 'z-perm.png', algorithms: ["(M2 U M2 U) M' (U2 M2 U2 M' U2)"] }
     ]
   };
 }]);
